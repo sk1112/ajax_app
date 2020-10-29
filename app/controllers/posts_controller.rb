d95dd1a8eb80358b342a3b2ac0f8d4b051a63cc5
sk1112 <72970375+sk1112@users.noreply.github.com>
2020-10-30 01:04:41 +0900

    メモ投稿機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,8 +3,8 @@ class PostsController < ApplicationController
     @posts = Post.all.order(id: "DESC")
   end
   def create
-    Post.create(content: params[:content])
-    redirect_to action: :index
+    post = Post.create(content: params[:content], checked: false)
+    render json:{ post: post }
   end
   def checked
     post = Post.find(params[:id])
