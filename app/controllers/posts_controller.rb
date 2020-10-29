9fbafe9e606797fae03145bef2b980ba0fd20aa0
sk1112 <72970375+sk1112@users.noreply.github.com>
2020-10-29 18:16:41 +0900

    既読機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,16 @@ class PostsController < ApplicationController
     Post.create(content: params[:content])
     redirect_to action :index
   end
+
+  def checked
+    post = Post.find(params[:id])
+    if post.checked
+      post.update(checked: false)
+    else
+      post.update(checked: true)
+    end
+
+    item = Post.find(params[:id])
+    render json: { post: item }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: 'posts#index'
   post 'posts', to: 'posts#create'
+  get 'posts/:id', to: 'posts#checked'
 end
