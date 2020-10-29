12d8417acc638af3e06fc390feb0de172f7b8b6a
sk1112 <72970375+sk1112@users.noreply.github.com>
2020-10-29 19:31:02 +0900

    既読機能のフロントエンド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,12 +2,10 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all.order(id: "DESC")
   end
-
   def create
     Post.create(content: params[:content])
-    redirect_to action :index
+    redirect_to action: :index
   end
-
   def checked
     post = Post.find(params[:id])
     if post.checked
@@ -15,8 +13,8 @@ class PostsController < ApplicationController
     else
       post.update(checked: true)
     end
-
     item = Post.find(params[:id])
     render json: { post: item }
   end
+
 end
---
+++ app/javascript/checked.js
@@ -0,0 +1,29 @@
+function check() {
+  const posts = document.querySelectorAll(".post");
+  posts.forEach(function (post) {
+    if (post.getAttribute("data-load") != null) {
+      return null;
+    }
+    post.setAttribute("data-load", "true");
+    post.addEventListener("click", () => {
+      const postId = post.getAttribute("data-id");
+      const XHR = new XMLHttpRequest();
+      XHR.open("GET", `/posts/${postId}`, true);
+      XHR.responseType = "json";
+      XHR.send();
+      XHR.onload = () => {
+        if (XHR.status != 200) {
+          alert(`Error ${XHR.status}: ${XHR.statusText}`);
+          return null;          
+        }
+        const item = XHR.response.post;
+        if (item.checked === true) {
+          post.setAttribute("data-check", "true");
+        } else if (item.checked === false) {
+          post.removeAttribute("data-check");
+        }
+      };
+    });
+  });
+}
+setInterval(check, 1000);
