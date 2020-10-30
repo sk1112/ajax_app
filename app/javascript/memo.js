0106c33bd74211922878cd88900d61e7a1f2d36f
sk1112 <72970375+sk1112@users.noreply.github.com>
2020-10-30 13:15:38 +0900

    メモ投稿機能のフロントエンド実装

---
+++ app/javascript/memo.js
@@ -0,0 +1,32 @@
+function memo() {
+  const submit = document.getElementById("submit");
+  submit.addEventListener("click", (e) => {
+    const formData = new FormData(document.getElementById("form"));
+    const XHR = new XMLHttpRequest();
+    XHR.open("POST", "/posts", true);
+    XHR.responseType = "json";
+    XHR.send(formData);
+    XHR.onload = () => {
+      if (XHR.status != 200) {
+        alert(`Error ${XHR.status}: ${XHR.statusText}`);
+        return null;
+      }
+      const item = XHR.response.post;
+      const list = document.getElementById("list");
+      const formText = document.getElementById("content");
+      const HTML = `
+        <div class="post" data-id=${item.id}>
+          <div class="post-date">
+            投稿日時：${item.created_at}
+          </div>
+          <div class="post-content">
+          ${item.content}
+          </div>
+        </div>`;
+      list.insertAdjacentHTML("afterend", HTML);
+      formText.value = "";
+    };
+    e.preventDefault();
+  });
+}
+window.addEventListener("load", memo);
